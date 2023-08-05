ea29b1d0bdadf4069c3042fd67188d22f45707ba
GEDEON KASHOMONA <68714616+GEDEON1708@users.noreply.github.com>
2023-08-05 03:31:50 -0300

    Add files via upload

---
+++ lampada.js
@@ -0,0 +1,15 @@
+lamp = document.getElementById("lampada");
+
+function Ligar() {
+  lamp.src = "./img/ligada.svg";
+}
+
+function Desligar() {
+  lamp.src = "./img/desligada.svg";
+}
+
+function Quebrar() {
+  lamp.src = "./img/quebrada.svg";
+}
+
+function Avisar() {}
